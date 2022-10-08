bf5471e3e1c65a4b0074b2380b1642c5f6877b68
shillbit <37974296+shillbit@users.noreply.github.com>
2022-10-08 17:28:49 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,15 +4,15 @@ $choco_destination = 'install-choco.ps1'
 Invoke-WebRequest -Uri $choco_source -OutFile $choco_destination
  
 ## Download Install Apps Script
-$apps_source = 'https://raw.githubusercontent.com/pukauw/azure-client/master/install-apps.ps1'
+$apps_source = 'https://raw.githubusercontent.com/pukauw/azure-vm/master/install-apps.ps1'
 $apps_destination = 'install-apps.ps1'
 
-## Download and Install Dev Cert
-$dev_source = 'https://raw.githubusercontent.com/pukauw/azure-client/master/dev.cer'
-$dev_destination = 'dev.cer'
+# ## Download and Install Dev Cert
+# $dev_source = 'https://raw.githubusercontent.com/pukauw/azure-vm/master/dev.cer'
+# $dev_destination = 'dev.cer'
 
 Invoke-WebRequest -Uri $apps_source -OutFile $apps_destination
-Invoke-WebRequest -Uri $dev_source -OutFile $dev_destination
+# Invoke-WebRequest -Uri $dev_source -OutFile $dev_destination
 
 ## Stop the Server Manager Window popping up
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose 
@@ -20,4 +20,4 @@ Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
 ## Install Choco
 powershell -NoProfile -ExecutionPolicy Unrestricted -File "./install-choco.ps1"
 #powershell -NoProfile -ExecutionPolicy Unrestricted -File "./install-apps.ps1" 
-certutil -enterprise -f -v -AddStore Root dev.cer
+# certutil -enterprise -f -v -AddStore Root dev.cer
